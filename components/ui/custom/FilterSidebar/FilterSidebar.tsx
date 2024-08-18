6dc31d5f6214f7b347ec6128e1986de28ddaf97b
vujic02 <vujicn02@gmail.com>
2024-08-18 22:05:00 +0200

    finished desktop version of the sidebar

--- components/ui/custom/FilterSidebar/FilterSidebar.tsx
+++ components/ui/custom/FilterSidebar/FilterSidebar.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import CustomSelect from "../Search/CustomSelect";
 import { customSelectData, customSelectDataDynamic } from "@/types/Home";
 import { carsMakeData, carsModelData, countries, firstRegistration, motorcycleMakeData, motorcycleModelData, prices } from "@/utils/tabsStatic";
+import { Badge } from "@/components/ui/badge";
 
 interface Props {}
 
@@ -17,22 +18,42 @@ const FilterSidebar = ({}: Props) => {
 
   return (
     <div className="bg-white px-4 py-6">
+      <div className="py-4 px-2 min-h-36">
+        <p className="text-base text-[#333] font-semibold mt-2">My search (2)</p>
+        <div className="flex flex-wrap gap-1 mt-2">
+          <Badge variant="secondary">Europe</Badge>
+          <Badge variant="secondary">BMW</Badge>
+        </div>
+      </div>
       <div className="border-t border-t-[#dcdcdc] py-2">
-        <label className="text-sm">Make</label>
-        <CustomSelect placeholder="Make" data={carsMakeData} setSelectedOption={setSelectedMake} />
-        <label className="text-sm">Model</label>
-        <CustomSelect
-          placeholder="Model"
-          disabled={selectedMake === "" ? true : false}
-          data={modelData && [modelData]}
-          setSelectedOption={setSelectedModel}
-        />
-        <label className="text-sm">Price</label>
-        <CustomSelect placeholder="Price up to (€)" data={prices} setSelectedOption={setSelectedPrice} />
-        <label className="text-sm">First registration</label>
-        <CustomSelect placeholder="First registration from" data={firstRegistration} setSelectedOption={setSelectedRegistration} />
-        <label className="text-sm">Countries</label>
-        <CustomSelect placeholder="Europe" data={countries} setSelectedOption={setSelectedCountry} />
+        <div className="flex flex-col gap-y-3 px-2 mt-2">
+          <p className="text-[#333] text-base font-semibold">Basic specifications & Location</p>
+          <div>
+            <label className="inline-block text-base mb-0.5 mt-6">Make</label>
+            <CustomSelect placeholder="Make" data={carsMakeData} setSelectedOption={setSelectedMake} />
+          </div>
+          <div>
+            <label className="inline-block text-base mb-0.5">Model</label>
+            <CustomSelect
+              placeholder="Model"
+              disabled={selectedMake === "" ? true : false}
+              data={modelData && [modelData]}
+              setSelectedOption={setSelectedModel}
+            />
+          </div>
+          <div>
+            <label className="inline-block text-base mb-0.5">Price</label>
+            <CustomSelect placeholder="Price up to (€)" data={prices} setSelectedOption={setSelectedPrice} />
+          </div>
+          <div>
+            <label className="inline-block text-base mb-0.5">First registration</label>
+            <CustomSelect placeholder="First registration from" data={firstRegistration} setSelectedOption={setSelectedRegistration} />
+          </div>
+          <div>
+            <label className="inline-block text-base mb-0.5">Countries</label>
+            <CustomSelect placeholder="Europe" data={countries} setSelectedOption={setSelectedCountry} />
+          </div>
+        </div>
       </div>
     </div>
   );
